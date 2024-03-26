301accfafc08816acf84affa5e136798aad563fb
Mikhail Nikitin <mikhail.nikitin92@gmail.com>
2024-03-27 01:42:36 +0300

    add basic implementation of LoRA adapter and its usage for QKV and Linear layers

---
+++ lora.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LoRALayer(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, rank, alpha):
+        super().__init__()
+        std = torch.sqrt(torch.tensor(rank).float())
+        self.A = torch.nn.Parameter(torch.randn(in_dim, rank) / std)
+        self.B = torch.nn.Parameter(torch.zeros(rank, out_dim))
+        self.alpha = alpha
+
+    def forward(self, x):
+        x = self.alpha * (x @ self.A @ self.B)
+        return x
+
+
+class QkvWithLoRA(torch.nn.Module):
+    def __init__(self, qkv, rank, alpha):
+        super().__init__()
+        self.qkv = qkv
+        self.dim = qkv.in_features
+        self.lora_q = LoRALayer(self.dim, self.dim, rank, alpha)
+        self.lora_v = LoRALayer(self.dim, self.dim, rank, alpha)
+
+    def forward(self, x):
+        qkv = self.qkv(x)
+        qkv[:, :, :self.dim] += self.lora_q(x)
+        qkv[:, :, -self.dim:] += self.lora_v(x)
+        return qkv
+
+
+class LinearWithLoRA(torch.nn.Module):
+    def __init__(self, linear, rank, alpha):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features, linear.out_features, rank, alpha
+        )
+
+    def forward(self, x):
+        return self.linear(x) + self.lora(x)
+
+
